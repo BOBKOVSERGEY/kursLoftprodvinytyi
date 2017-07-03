cdd629ec102ba5f04a87418394aae725af089a3a
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-07-03 21:02:14 +0300

    Анимация с callback функцией

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var bs = require("browser-sync").create();
 
-bs.watch(['adaptiv/*.*', 'adaptiv/css/*.css']).on("change", bs.reload);
+bs.watch(['jquery/*.*']).on("change", bs.reload);
 
 // Start a Browsersync proxy
 bs.init({
-  proxy: "kursLoftprodvinytyi/adaptiv"
+  server: "./jquery"
 });
---
+++ jquery/main.js
@@ -0,0 +1,8 @@
+$(function () {
+  $('.block').animate({
+    'width' : '200px',
+    'height' : '200px',
+  }, 1400, function () {
+    alert('hellow');
+  });
+})
