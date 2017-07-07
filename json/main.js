b4b746a415b242737b8b6256352b2149fcf0a5ad
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-07-07 13:56:23 +0300

    json

--- json/main.js
+++ json/main.js
@@ -8,3 +8,13 @@ var jsonData = {
 
 console.log(jsonData);
 console.log(JSON.stringify(jsonData)); // преобразум в строку
+
+var result = JSON.stringify(jsonData, ['name', 'age']);
+
+
+var el = document.querySelector('.section').innerHTML = result;
+
+var resultobj = JSON.parse(result, function (key, val) {
+  console.log(key, val);
+}); // возвращает опять объект
+
