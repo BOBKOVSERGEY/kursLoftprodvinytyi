51ec511076de44c7ff238ae1707bffb33491f636
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-07-07 13:00:07 +0300

    Преобразуем json формат в строку

---
+++ json/main.js
@@ -0,0 +1,10 @@
+
+var jsonData = {
+  "name" : "Zar",
+  "lastname" : "Zakharov",
+  "age" : 28,
+  "maried" : true
+}
+
+console.log(jsonData);
+console.log(JSON.stringify(jsonData)); // преобразум в строку
