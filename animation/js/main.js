04253cab360fe6299272b9f7b9f4ad3fdece0893
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-07-27 08:09:23 +0300

    blur

--- animation/js/main.js
+++ animation/js/main.js
@@ -20,4 +20,26 @@ $(window).scroll(function () {
     }
 
   }());
-})
+}); //-> scroll_end
+
+$(document).ready(function () {
+  setBlur();
+}); // -> ready_end;
+
+$(window).resize(function () {
+  setBlur();
+});//-> resize_end
+
+function setBlur() {
+  var
+    imgWidth = $('.blur__background').width(),
+    blur = $('.blur__form'),
+    blurSection = $('.blur'),
+    posTop = blurSection.offset().top - blur.offset().top,
+    posLeft = blurSection.offset().left - blur.offset().left;
+
+  blur.css({
+    'background-size' : imgWidth + 'px' + ' ' + 'auto',
+    'background-position' : posLeft + 'px' + ' ' + posTop + 'px'
+  })
+}
