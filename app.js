fa0c471490ed5f1be220688196623514816c2612
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-08-17 09:10:18 +0300

    Вставка картинки js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var bs = require("browser-sync").create();
 
-bs.watch(['javascript2/*.*', 'javascript2/css/*.css', 'javascript2/js/*.js']).on("change", bs.reload);
+bs.watch(['javascript2/test/*.*']).on("change", bs.reload);
 
 // Start a Browsersync proxy
 bs.init({
-  proxy: "kursLoftprodvinytyi/javascript2"
+  proxy: "kursLoftprodvinytyi/javascript2/test"
 });
---
+++ javascript2/test/index.js
@@ -0,0 +1,6 @@
+
+// https://wallpaperscraft.ru/image/kosmos_nebo_3d_art_97531_1920x1200.jpg
+
+var image = new Image();
+document.body.appendChild(image);
+image.src = 'https://wallpaperscraft.ru/image/kosmos_nebo_3d_art_97531_1920x1200.jpg';
