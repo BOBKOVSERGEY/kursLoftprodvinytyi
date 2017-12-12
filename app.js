0947a44a159193f63cebabb997d2f2c17fe242f4
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-12-12 10:24:00 +0300

    flex-wrap: wrap - перенос на другую строку, если не вмещается

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var bs = require("browser-sync").create();
 
-bs.watch(['nodejs/*.*']).on("change", bs.reload);
+bs.watch(['./flexbox/*.*', './flexbox/style.scss', './flexbox/style.css']).on("change", bs.reload);
 
 // Start a Browsersync proxy
 bs.init({
-  proxy: "kursLoftprodvinytyi/nodejs"
+  server: "./flexbox"
 });
