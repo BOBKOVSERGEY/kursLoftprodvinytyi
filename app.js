1a38dcad39ff85089be6c21bada9e3c5812bab14
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-12-14 12:02:04 +0300

    End flexbox

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var bs = require("browser-sync").create();
 
-bs.watch(['./flexbox/*.*', './flexbox/style.scss', './flexbox/style.css']).on("change", bs.reload);
+bs.watch(['./flexbox/app/*.*', './flexbox/app/css/style.scss', './flexbox/app/css/style.css']).on("change", bs.reload);
 
 // Start a Browsersync proxy
 bs.init({
-  server: "./flexbox"
+  server: "./flexbox/app"
 });
